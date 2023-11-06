35e3069061e47f11afbed8e779b5325ede29b1c3
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-06 11:05:17 -0800

    Update A-rule-of-divisibility-by-13.js

--- A-rule-of-divisibility-by-13.js
+++ A-rule-of-divisibility-by-13.js
@@ -44,3 +44,9 @@ Task:
 
 
 // Solution
+
+function thirt(n) {
+  const nums = [1,10,9,12,3,4]
+  let sum = (''+n).split('').reverse().reduce((sum,v,i) => sum + v * nums[i%nums.length], 0)
+  return sum === n ? n : thirt(sum)
+}
